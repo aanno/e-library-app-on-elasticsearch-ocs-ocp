c576371d780d24725720bc75649bfea2070d3689
aanno <aanno@users.noreply.github.com>
2023-10-24 15:25:49 +0200

    converted  4_deploy_frontend_app.yaml into template

--- deploy-on-ocp/deploy_all_in_one.sh
+++ deploy-on-ocp/deploy_all_in_one.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-oc create -f 1_create_ns_ocs_pvc.yaml
-oc create -f 2_deploy_elasticsearch.yaml
-oc create -f 3_deploy_backend_api.yaml
+OC_PROJECT="e-library"
+
+# this must be done as Administrator
+oc create -f 1_create_ns_ocs_pvc.yaml -p OC_PROJECT=${OC_PROJECT}
+
+# this can be done as developer
+oc create -f 2_deploy_elasticsearch.yaml -p OC_PROJECT=${OC_PROJECT}
+oc create -f 3_deploy_backend_api.yaml -p OC_PROJECT=${OC_PROJECT}
 echo "env = {BACKEND_URL: 'http://$(oc get route -n e-library -o=jsonpath="{.items[0]['spec.host']}")'}" > env.js
 oc create configmap -n e-library env.js --from-file=env.js
-oc create -f 4_deploy_frontend_app.yaml
+oc create -f 4_deploy_frontend_app.yaml -p OC_PROJECT=${OC_PROJECT}
+
 echo "Ingesting dataset (100 classic novels) on to Elasticsearch ..... "
 sleep 10
+
 kubectl exec -it e-library-backend-api  -n e-library -- curl -X POST http://localhost:3000/load_data
 oc get route frontend -n e-library
 
